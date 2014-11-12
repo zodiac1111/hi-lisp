cd21f29febf344334f7fa79f531dcd437276ad3e
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-11-12 22:54:10 +0800

    http测试

--- http.lisp
+++ http.lisp
@@ -1,11 +1,12 @@
 ;;;; quicklisp 一个好用的库管理程序
 (load "quicklisp.lisp")
+
+
 ;;;; 加载一个http实现
 (ql:quickload :drakma)
-
 ;;;; 这个http实现输出定向到标准输出
 (setf drakma:*header-stream* *standard-output*)
 
-;; 发送请求,会向标准输出输出字符
-(drakma:http-request "http://google.com/")
+;;;; 使用这个库:发送请求,会向标准输出输出字符
+(drakma:http-request "http://163.com/")
 (format t "aaaa")
